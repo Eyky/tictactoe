afd2a765957f340f45405f15aa56400d77789fa6
Eyky <eirikurbe10@ru.is>
2014-11-30 23:41:23 +0000

    noobing around

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+echo Cleaning...
+rm -rf ./dist
+ 
+echo "Installing grunt"
+npm install grunt
+ 
+echo "Installing bower"
+npm install bower
+ 
+echo "Npm install"
+npm install
+ 
+echo "Bower install"
+bower install
+ 
+ 
+echo "Running grunt"
+grunt
+ 
+echo "Done"

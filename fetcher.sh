87c975484d2ab0874dc03610ced16eff0da1a193
Eyky <eirikurbe10@ru.is>
2014-12-01 15:30:32 +0000

    last try!

--- fetcher.sh
+++ fetcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "Stopping and removing all docker containers"
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
@@ -8,5 +7,5 @@ echo "Pulling new docker image"
 docker pull eykyeyky/tictactoe
 echo "Starting server"
 docker run -p 80:8080 -d -e "NODE_ENV=production" eykyeyky/tictactoe
-echo "Closing ssh"
+echo "Closing ssh!"
 exit

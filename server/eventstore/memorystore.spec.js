d64ee5570c81ba89174a92eea3266d10dd4ca9be
eyky <eirikurbe10@ru.is>
2014-12-05 21:51:17 +0000

    to test if an event stored can be loaded again

--- server/eventstore/memorystore.spec.js
+++ server/eventstore/memorystore.spec.js
@@ -5,6 +5,21 @@ var memoryStore = require('./memorystore');
 var should = require('should');
 
 describe('In memory event store', function(){
+
+
+  var createGameEvent = [
+    {
+      event: "GameCreated",
+      user: {
+        userName: "Eyky"
+      },
+      id: "1",
+      name: "LeGame",
+      timeStamp: "2014-12-02T11:29:29"
+    }
+  ];
+
+
   it('Should return empty array for unkown id', function(){
 
     var store = memoryStore();
@@ -15,5 +30,18 @@ describe('In memory event store', function(){
     should(loadedEvents).be.instanceof(Array);
   });
 
+  it('Should return events previously stored', function(){
+
+
+
+
+    var store = memoryStore();
+
+    store.storeEvents('1234', createGameEvent );
+
+    var loadedEvents = store.loadEvents('1234');
+
+    should(loadedEvents).eql(createGameEvent);
+  });
 
 })

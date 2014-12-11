811f2afceec16066436677a8d824cb08924d929b
eyky <eirikurbe10@ru.is>
2014-12-11 22:14:31 +0000

    refactoring because NOT TDD

--- client/app/tictactoe/CreateGame/tictactoeCreateGame.controller.js
+++ client/app/tictactoe/CreateGame/tictactoeCreateGame.controller.js
@@ -1,35 +1,47 @@
 angular.module('tictactoeApp')
   .controller('TictactoeController', function ($scope, $state, $http, TicTacToeService) {
-    "use strict";
+    'use strict';
+
+
+    function generateUUID(){
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c==='x' ? r : (r&0x3|0x8)).toString(16);
+      });
+      return uuid;
+    }
+
 
 
     //$scope.playGame = false;
     //$scope.fneh = true;
     var genId = generateUUID();
+    TicTacToeService.setUUID(genId);
 
     $scope.processEvents = function(events){
       $scope.processedEvents = events;
-        console.log(events);
+
         TicTacToeService.setPlayerName($scope.userName);
         TicTacToeService.setPlayerOne(TicTacToeService.getPlayerName());
         TicTacToeService.setGameJoined(true);
         TicTacToeService.setPlayerSymbol(events[0].event);
         TicTacToeService.setUUID(genId);
-        console.log(TicTacToeService.getUUID());
+
         $state.go('playgame/:id', {id: events[0].id});
 
     };
 
     $scope.createGame = function(){
-      console.log("fneh");
       var postPromise = $http.post('/api/createGame/',{
-          "id":genId,
+          "id":TicTacToeService.getUUID(),
           "cmd":"CreateGame",
           "user":{
             "userName":$scope.userName
           },
           "name":$scope.name,
-          "timeStamp":"2014-12-02T11:29:29"
+          "timeStamp":TicTacToeService.getNewDate()
         }
       );
       postPromise.then(function(data){
@@ -40,12 +52,4 @@ angular.module('tictactoeApp')
   });
 
 
-function generateUUID(){
-  var d = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = (d + Math.random()*16)%16 | 0;
-    d = Math.floor(d/16);
-    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-  });
-  return uuid;
-};
+
---
+++ client/app/tictactoe/CreateGame/tictactoeCreateGame.controller.spec.js
@@ -0,0 +1,78 @@
+/**
+ * Created by eyky on 11.12.2014.
+ */
+
+//yes I know, this was not TDD.
+
+'use strict';
+describe('Controller: TictactoeController', function() {
+
+  // load the controller's module
+  beforeEach(module('tictactoeApp'));
+
+  var TictactoeControllerCtrl, scope, httpBackend, http;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ( $injector, $controller, $rootScope, $http, TicTacToeService) {
+    http = $http;
+    httpBackend = $injector.get('$httpBackend');
+
+    TicTacToeService.getNewDate = function () {
+      return "2014-01-01T03:06:00";
+    };
+
+    TicTacToeService.getUUID = function () {
+      return "1";
+    }
+    var genId = 1;
+    scope = $rootScope.$new();
+    TictactoeControllerCtrl = $controller('TictactoeController', {
+      $scope: scope
+    });
+  }));
+
+
+  afterEach(function () {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
+
+  it('should emit a creategame event when putton is pushed', function(){
+
+
+    httpBackend.expectPOST('/api/createGame/', {
+      id:"1",
+      cmd: "CreateGame",
+      user:{
+        userName:"Eyky"
+      },
+      name:"leGame",
+      timeStamp:"2014-01-01T03:06:00"
+    }).respond(
+      [
+        {
+          id:"1",
+          event: "GameCreated",
+          user:{
+            userName:"Eyky"
+          },
+          name:"leGame",
+          timeStamp:"2014-01-01T03:06:00"
+        }
+      ]
+    );
+
+
+    scope.name = "leGame";
+    scope.userName = "Eyky"
+
+    scope.createGame();
+
+    httpBackend.expectGET('app/main/main.html').respond('');
+    httpBackend.expectGET('app/tictactoe/tictactoegame.html').respond('');
+    httpBackend.flush();
+    expect(scope.processedEvents.length).toBe(1);
+  });
+
+});

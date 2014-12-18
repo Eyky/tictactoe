81800b2b20e157746719d156db6a751b80f5fd1e
eyky <eirikurbe10@ru.is>
2014-12-18 18:05:58 +0000

    uppig browser sleep time to 2000

--- e2e/main/creategame.spec.js
+++ e2e/main/creategame.spec.js
@@ -50,9 +50,9 @@ describe('Tictactoe game play', function() {
         game.waitForTictactoePage();
         game.expectGameBoardShowing();
         game.expectFirstCellShowing();
-        browser.sleep(1000);
+        browser.sleep(2000);
         tictactoe.cell0.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell0.getText()).toBe('X');
 
 
@@ -68,39 +68,39 @@ describe('Tictactoe game play', function() {
 
 
             browser.switchTo().window('second-window');
-            browser.sleep(1000);
+            browser.sleep(2000);
             game.joinGameName("ben");
             game.joinGame();
-            browser.sleep(1000);
+            browser.sleep(2000);
             tictactoe.cell8.click();
-            browser.sleep(1000);
+            browser.sleep(2000);
             expect(tictactoe.cell8.getText()).toBe('O');
 
             browser.switchTo().window(firstwindow);
             tictactoe.cell4.click();
-            browser.sleep(1000);
+            browser.sleep(2000);
             expect(tictactoe.cell4.getText()).toBe('X');
 
             browser.switchTo().window('second-window');
             tictactoe.cell2.click();
-            browser.sleep(1000);
+            browser.sleep(2000);
             expect(tictactoe.cell2.getText()).toBe('O');
 
             browser.switchTo().window(firstwindow);
             tictactoe.cell3.click();
-            browser.sleep(1000);
+            browser.sleep(2000);
             expect(tictactoe.cell3.getText()).toBe('X');
 
             browser.switchTo().window('second-window');
             tictactoe.cell5.click();
-            browser.sleep(1000);
+            browser.sleep(2000);
             expect(tictactoe.cell5.getText()).toBe('O');
             expect(tictactoe.result.getText()).toBe('You Win');
 
             browser.switchTo().window(firstwindow);
             expect(tictactoe.result.getText()).toBe('You lose');
             tictactoe.cell6.click();
-            browser.sleep(1000);
+            browser.sleep(2000);
             expect(tictactoe.cell6.getText()).toBe(' ');
 
 
@@ -118,9 +118,9 @@ describe('Tictactoe game play', function() {
     game.waitForTictactoePage();
     game.expectGameBoardShowing();
     game.expectFirstCellShowing();
-    browser.sleep(1000);
+    browser.sleep(2000);
     tictactoe.cell4.click();
-    browser.sleep(1000);
+    browser.sleep(2000);
     expect(tictactoe.cell4.getText()).toBe('X');
 
     browser.getCurrentUrl().then(function(url) {
@@ -131,50 +131,50 @@ describe('Tictactoe game play', function() {
         browser.executeScript('window.open("'+ url +'", "second-window","status=yes,toolbar=no,menubar=no,location=no")');
 
         browser.switchTo().window('second-window');
-        browser.sleep(1000);
+        browser.sleep(2000);
         game.joinGameName("ben");
         game.joinGame();
-        browser.sleep(1000);
+        browser.sleep(2000);
 
 
         tictactoe.cell0.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell0.getText()).toBe('O');
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell1.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell1.getText()).toBe('X');
 
         browser.switchTo().window('second-window');
         tictactoe.cell7.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell7.getText()).toBe('O');
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell6.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell6.getText()).toBe('X');
 
         browser.switchTo().window('second-window');
         tictactoe.cell2.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell2.getText()).toBe('O');
 
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell3.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell3.getText()).toBe('X');
 
         browser.switchTo().window('second-window');
         tictactoe.cell8.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell8.getText()).toBe('O');
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell5.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell5.getText()).toBe('X');
         expect(tictactoe.result.getText()).toBe('You Win');
 
@@ -204,9 +204,9 @@ describe('Tictactoe game play', function() {
     game.waitForTictactoePage();
     game.expectGameBoardShowing();
     game.expectFirstCellShowing();
-    browser.sleep(1000);
+    browser.sleep(2000);
     tictactoe.cell0.click();
-    browser.sleep(1000);
+    browser.sleep(2000);
     expect(tictactoe.cell0.getText()).toBe('X');
 
     browser.getCurrentUrl().then(function(url) {
@@ -217,50 +217,50 @@ describe('Tictactoe game play', function() {
         browser.executeScript('window.open("'+ url +'", "second-window","status=yes,toolbar=no,menubar=no,location=no")');
 
         browser.switchTo().window('second-window');
-        browser.sleep(1000);
+        browser.sleep(2000);
         game.joinGameName("ben");
         game.joinGame();
-        browser.sleep(1000);
+        browser.sleep(2000);
 
 
         tictactoe.cell4.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell4.getText()).toBe('O');
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell1.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell1.getText()).toBe('X');
 
         browser.switchTo().window('second-window');
         tictactoe.cell7.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell7.getText()).toBe('O');
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell6.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell6.getText()).toBe('X');
 
         browser.switchTo().window('second-window');
         tictactoe.cell2.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell2.getText()).toBe('O');
 
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell5.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell5.getText()).toBe('X');
 
         browser.switchTo().window('second-window');
         tictactoe.cell3.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell3.getText()).toBe('O');
 
         browser.switchTo().window(firstwindow);
         tictactoe.cell8.click();
-        browser.sleep(1000);
+        browser.sleep(2000);
         expect(tictactoe.cell8.getText()).toBe('X');
         expect(tictactoe.result.getText()).toBe('Draw');
 
